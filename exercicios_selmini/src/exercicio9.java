e56e1ffaad845fcde1670f6d3ef7ea434d9cd6e9
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 20:50:54 -0300

    exercicio9

--- exercicios_selmini/src/exercicio9.java
+++ exercicios_selmini/src/exercicio9.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class exercicio9 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println(" Escreva seu numero: ");
+
+        int numero = teclado.nextInt();
+
+        int unidade = numero%10;
+        numero /=10;
+        int dezena = numero%10;
+        numero /=10;
+        int centena = numero%10;
+
+        int invertido = unidade*100 + dezena*10 + centena;
+        System.out.println(" Seu numero inverso é: " + invertido);
+
+
+
+    }
 }
